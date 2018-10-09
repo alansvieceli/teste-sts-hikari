23209b8038f2a081b32d47b4f14174c66d23125d
Alan Vieceli <alan.vieceli@softplan.com.br>
2018-10-09 12:39:00 -0300

    Teste com Hikari

---
+++ src/test/java/com/alanvieceli/api/TesteHikariApplicationTests.java
@@ -0,0 +1,29 @@
+package com.alanvieceli.api;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteHikariApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+	
+	@Test
+	public void contextLoads() {
+	}
+	
+    @Test
+    public void hikariConnectionPoolIsConfigured() {    	
+        assertEquals("com.zaxxer.hikari.HikariDataSource", dataSource.getClass().getName());
+    }
+
+}
